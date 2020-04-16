f6e83bd639c9f93307907927cdcefbd733a4e389
szattila98 <clownface68@gmail.com>
2020-04-16 15:00:22 +0200

    Transformed the script to take screenshots in different browsers. Added requirements.txt. Added readme.

---
+++ README.md
@@ -0,0 +1,22 @@
+## Info
+
+This script takes two screenshots with the help of Selenium automation framework, one with the chrome driver, one with the firefox driver and compares them. The results will be printed as an image with red squares to show different areas.
+
+## Usage
+
+- It is recommended to activate venv and then install the requirements.
+
+- The script also requires the used browser engines to be installed and added to PATH. <br>
+(If you use chocolatey -> https://chocolatey.org/packages/selenium-all-drivers)
+
+- Change url constants to compare screenshots of the pages you would like to check out.
+
+- Remove 'headless' options in the 'set_up' function to see the magic as it happens.
+
+- To compare custom pictures just put screenshots with the correct name into the 'screenshots' folder and comment the 'capture_screens' function.
+
+## How it works
+
+Comparation is done by calculating average brightness per square and it draws the squares which are different.
+
+## [Based on](https://blog.rinatussenov.com/automating-manual-visual-regression-tests-with-python-and-selenium-be66be950196)
--- ScreenAnalysis.py
+++ ScreenAnalysis.py
@@ -3,73 +3,79 @@ from selenium import webdriver
 import os
 import sys
 
-# Use following drivers with selenium, without a driver it won't work -> https://chocolatey.org/packages/selenium-all-drivers
-# Multiple drivers can be used, just set up more and take the pics with different ones.
-# To use manually, comment out the capture function call, and put images into the screenshot dir with the correct names.
-# Take out headless options to see the magic happen with your own eyes.
-
 class ScreenAnalysis:
 
-    STAGING_URL = 'https://www.uni-miskolc.hu/'
-    PRODUCTION_URL = 'https://www.uni-miskolc.hu/'
-    options = None
-    driver = None
+    CHROME_URL = 'https://www.uni-miskolc.hu/'
+    FIREFOX_URL = 'https://www.uni-miskolc.hu/'
+    ChromeDriver = None
+    FirefoxDriver = None
 
     def __init__(self):
         os.makedirs('./screenshots', exist_ok=True)
         self.set_up()
-        self.capture_screens() # comment this to use manually
+        self.capture_screens() # Comment for manual use
         self.analyze()
         self.clean_up()
 
     def set_up(self):
         # Chrome
-        self.options = webdriver.ChromeOptions()
-        self.options.add_argument('headless')
-        self.options.add_argument("start-maximized");
-        self.driver = webdriver.Chrome(options=self.options)
-
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless') # Headless option
+        options.add_argument("start-maximized");
+        self.ChromeDriver = webdriver.Chrome(options=options)
         # Firefox
-        # self.options = webdriver.FirefoxOptions()
-        # self.options.headless = True
-        # self.driver = webdriver.Firefox(options=self.options)
-        # self.driver.maximize_window()
+        options = webdriver.FirefoxOptions()
+        options.headless = True # Headless option
+        self.FirefoxDriver = webdriver.Firefox(options=options)
+        self.FirefoxDriver.maximize_window()
 
     def clean_up(self):
-        self.driver.close()
+        self.ChromeDriver.close()
+        self.FirefoxDriver.close()
 
     def capture_screens(self):
-        self.screenshot(self.STAGING_URL, 'screen_staging.png')
-        self.screenshot(self.PRODUCTION_URL, 'screen_production.png')
+        self.screenshot(self.CHROME_URL, 'screenshot_chrome.png', 'chrome')
+        self.screenshot(self.FIREFOX_URL, 'screenshot_firefox.png', 'firefox')
 
-    def screenshot(self, url, file_name):
-        print("Capturing", url, "screenshot as", file_name, "...")
-        self.driver.get(url)
-        self.driver.set_window_size(1920, 1080)
-        self.driver.save_screenshot(os.path.join('./', 'screenshots', file_name))
-        self.driver.get_screenshot_as_png()
-        print("Done.")
-
+    def screenshot(self, url, file_name, driver_type):
+        if (driver_type == 'chrome'):
+            print("Capturing", url, "screenshot as", file_name, "with chrome...")
+            self.ChromeDriver.get(url)
+            self.ChromeDriver.set_window_size(1920, 1080)
+            self.ChromeDriver.save_screenshot(os.path.join('./', 'screenshots', file_name))
+            self.ChromeDriver.get_screenshot_as_png()
+            print("Done.")
+        elif (driver_type == 'firefox'):
+            print("Capturing", url, "screenshot as", file_name, "with firefox ...")
+            self.FirefoxDriver.get(url)
+            self.FirefoxDriver.set_window_size(1920, 1080)
+            self.FirefoxDriver.save_screenshot(os.path.join('./', 'screenshots', file_name))
+            self.FirefoxDriver.get_screenshot_as_png()
+            print("Done.")
+        
+        
     def analyze(self):
-        screenshot_staging = Image.open("screenshots/screen_staging.png")
-        screenshot_production = Image.open("screenshots/screen_production.png")
+        print("Analyzing screenshots...")
+        screenshot_chrome = Image.open("screenshots/screenshot_chrome.png")
+        screenshot_firefox = Image.open("screenshots/screenshot_firefox.png")
         columns = 60
         rows = 80
-        screen_width, screen_height = screenshot_staging.size
+        screen_width, screen_height = screenshot_chrome.size
 
         block_width = ((screen_width - 1) // columns) + 1 # this is just a division ceiling
         block_height = ((screen_height - 1) // rows) + 1
 
         for y in range(0, screen_height, block_height+1):
             for x in range(0, screen_width, block_width+1):
-                region_staging = self.process_region(screenshot_staging, x, y, block_width, block_height)
-                region_production = self.process_region(screenshot_production, x, y, block_width, block_height)
+                region_staging = self.process_region(screenshot_chrome, x, y, block_width, block_height)
+                region_production = self.process_region(screenshot_firefox, x, y, block_width, block_height)
 
                 if region_staging is not None and region_production is not None and region_production != region_staging:
-                    draw = ImageDraw.Draw(screenshot_staging)
+                    draw = ImageDraw.Draw(screenshot_chrome)
                     draw.rectangle((x, y, x+block_width, y+block_height), outline = "red")
 
-        screenshot_staging.save("screenshots/result.png")
+        screenshot_chrome.save("screenshots/result.png")
+        print("Result screenshot saved!")
 
     def process_region(self, image, x, y, width, height):
         region_total = 0
